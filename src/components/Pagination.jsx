43b506545e7fed4fc2258ae03c7d8a9305b7dc59
djolex999 <djmarkovic999@gmail.com>
2023-11-13 16:06:34 +0100

    optimized pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,23 +1,25 @@
-const Pagination = ({ showsPerPage, totalShows, paginate }) => {
+const Pagination = ({ totalShows, showsPerPage, paginate }) => {
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalShows / showsPerPage); i++) {
-    pageNumbers.push(i);
+  for (let i = 0; i < Math.ceil(totalShows / showsPerPage); i++) {
+    pageNumbers.push(i + 1);
   }
 
   return (
     <nav>
       <ul className="flex flex-wrap justify-center mt-10">
-        {pageNumbers.map((number) => (
-          <li key={number} className="list-none mx-1 mb-2">
-            <button
-              onClick={() => paginate(number)}
-              className="py-2 px-4 rounded bg-slate-500 text-white hover:bg-slate-900 text-xs md:text-sm lg:text-base"
-            >
-              {number}
-            </button>
-          </li>
-        ))}
+        {pageNumbers.map((number) => {
+          return (
+            <li key={number} className="list-none mx-1 mb-2">
+              <button
+                onClick={() => paginate(number)}
+                className={`py-2 px-4 rounded bg-slate-500 text-white hover:bg-slate-900 text-xs md:text-sm lg:text-base`}
+              >
+                {number}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
